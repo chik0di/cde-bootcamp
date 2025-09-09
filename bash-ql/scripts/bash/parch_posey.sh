4ec3c82adca0434158079fd133b3f780d6f62dd2
chik0di <chikodiobu@gmail.com>
2025-09-09 05:01:35 -0700

    Update Script

--- bash-ql/scripts/bash/parch_posey.sh
+++ bash-ql/scripts/bash/parch_posey.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
-BASE_URL="https://raw.githubusercontent.com/jdbarillas/parchposey/master/data-raw"
+# BASE_URL="https://raw.githubusercontent.com/jdbarillas/parchposey/master/data-raw"
 
-FILES=(
-    "accounts.csv"
-    "orders.csv"
-    "region.csv"
-    "sales_reps.csv"
-    "web_events.csv"
-)
+# FILES=(
+#    "accounts.csv"
+#    "orders.csv"
+#    "region.csv"
+#    "sales_reps.csv"
+#    "web_events.csv"
+#)
 
-DATASET="$HOME/cde/bash-ql/pipeline/parch-posey_data" 
-mkdir -p $DATASET
+#DATASET="$HOME/cde/bash-ql/pipeline/parch-posey_data" 
+#mkdir -p $DATASET
 
-for f in "${FILES[@]}"; do
-	echo "Downloading $f..."
-	curl -o "$DATASET/$f" "$BASE_URL/$f"
-done
+#for f in "${FILES[@]}"; do
+#	echo "Downloading $f..."
+#	curl -o "$DATASET/$f" "$BASE_URL/$f"
+#done
+
+echo "Something wrong with github raw file content display so we're loading with db file directly"
+
+DATABASE="posey"
+DUMP="$HOME/cde/bash-ql/scripts/sql/parch-posey-dump.sql"
+
+if sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw "$DATABASE"; then
+    echo "Database '$DATABASE' already exists. Skipping creation."
+else
+    echo "Creating database: $DATABASE ..."
+    sudo -u postgres createdb "$DATABASE"
+fi
+
+echo "Loading SQL dump into $DATABASE ..."
+if sudo -u postgres psql -d "$DATABASE" -f "$DUMP"; then
+    echo "Data loaded into "$DATABASE"."
+else
+    echo "Failed to load data into the "$DATABASE" db"
+	exit 1
+fi
