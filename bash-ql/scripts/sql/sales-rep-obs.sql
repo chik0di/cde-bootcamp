cf077004caa2e8bee03ceef56ab4761ef5d63a9c
chik0di <chikodiobu@gmail.com>
2025-09-10 03:26:52 -0700

    Posey DB query reports for Manager Ayoola

--- bash-ql/scripts/sql/sales-rep-obs.sql
+++ bash-ql/scripts/sql/sales-rep-obs.sql
@@ -1,1 +1,13 @@
 /* Provide a table that shows the region for each sales rep along with their associated accounts. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) by account name. */
+
+SELECT 
+    r.name AS region_name, 
+    s.name AS sales_rep_name, 
+    a.name AS account_name
+FROM sales_reps s, 
+    region r,
+    accounts a 
+WHERE 
+    s.region_id = r.id AND 
+    s.id = a.sales_rep_id
+ORDER BY a.name
