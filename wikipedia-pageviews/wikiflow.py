cd380613a337403a7f88e8c83e4770bd451653a4
chik0di <chikodiobu@gmail.com>
2025-10-24 05:10:16 +0100

    Update DAG

--- wikipedia-pageviews/wikiflow.py
+++ wikipedia-pageviews/wikiflow.py
@@ -6,15 +6,15 @@ from wikimedia.scripts.wikitran import select_companies
 from wikimedia.scripts.sqlite_dump import load_to_sqlite
 from wikimedia.scripts.query_db import query_top_company
 
-# default_args = {
-#     'owner':'chik0di',
-#     'retries':2,
-#     'retry_delay':timedelta(minutes=1)
-# }
+default_args = {
+    'owner':'chik0di',
+    'retries':2,
+    'retry_delay':timedelta(minutes=1)
+}
 
 with DAG(
     dag_id="WikiPageViewCount",
-    # default_args=default_args,
+    default_args=default_args,
     start_date=datetime(2025, 10, 19),
     schedule=None,
     catchup=False
